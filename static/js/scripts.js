9d30a46bb7beb5c1ccc92134e5d07b6dd684ff00
TofunmiSodimu <35805326+TofunmiSodimu@users.noreply.github.com>
2024-02-13 21:10:30 -0500

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'projects']
+const section_names = ['home', 'projects', 'work', 'publications']
 
 
 window.addEventListener('DOMContentLoaded', event => {
